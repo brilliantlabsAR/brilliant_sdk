7dfb66fbd11cd901f152b4f69cffccf935816283
blakesu <2639376969@qq.com>
2025-09-04 10:53:29 +0800

    Fix the display color of hello_world.py

--- packages/frame_ble/examples/hello_world.py
+++ packages/frame_ble/examples/hello_world.py
@@ -16,7 +16,7 @@ async def main():
         # wait for a printed string to come back from Frame to ensure the Lua has executed, not just that the command was sent successfully
         if frame._type == BrilliantDeviceType.HALO:
             await frame.send_lua("frame.display.clear(0x000000);print(0)", await_print=True)
-            await frame.send_lua("frame.display.text('Hello, Frame!', 50, 50, 0x000000);print(0)", await_print=True)
+            await frame.send_lua("frame.display.text('Hello, Frame!', 50, 50, 0xffffff);print(0)", await_print=True)
             #await frame.send_lua("frame.display.clear(0xFF0000)", await_print=False)
         else:
             await frame.send_lua("frame.display.text('Hello, Frame!', 1, 1);frame.display.show();print(0)", await_print=True)
