97bfe4f659b48654c62b5b8cd2b1f278b8546c14
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2025-11-19 13:29:57 +1100

    python: updated pcm playback script

--- python/packages/frame_ble/tests/test_speaker_pcm.py
+++ python/packages/frame_ble/tests/test_speaker_pcm.py
@@ -9,26 +9,28 @@ async def main():
     b = FrameBle()
     await b.connect()
 
+    b._user_print_response_handler = print
+
     # 1. Configure the speaker in pcm mode
-    await b.send_lua("frame.speaker.start{encoder='pcm', sample_rate=8000, is_signed = 1, bit_depth=8, channels=1}")
-    await b.send_lua("frame.speaker.volume(10)")
+    await b.send_lua("frame.speaker.start{encoder='pcm', sample_rate=8000, is_signed = 1, bit_depth=8, channels=1};print(0)", await_print=True)
+    await b.send_lua("frame.speaker.volume(50);print(1)", await_print=True)
+    await b.send_lua("print(frame.bluetooth.max_length())", await_print=True)
+    print(b.max_data_payload())
 
 
     with open("tests/audio/female_w1_8k_s8.pcm", "rb") as f:
         data = f.read()
 
-    frame_size = 320
+    frame_size = 400
 
     # 3. Send and play frame by frame
     for i in range(0, len(data), frame_size):
         frame = data[i:i + frame_size]
-        # lua_hex = bin2lua_hex(frame)
-        # await b.send_lua(f"frame.speaker.play({lua_hex})")
         await b.send_audio(frame)
-        # await asyncio.sleep(0.01)  # 10ms playback interval (adjust based on actual latency)
+        await asyncio.sleep(0.05) # 8000 bytes/second = 1/20 second (should be 0.05)
 
     # 4. Stop playback
-    await b.send_lua("frame.speaker:stop()")
+    await b.send_lua("frame.speaker.stop();print(2)", await_print=True)
     await asyncio.sleep(1.0)
 
     await b.disconnect()
