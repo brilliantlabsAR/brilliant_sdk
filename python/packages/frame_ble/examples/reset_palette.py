881214db09b2af65e6f8142edf0334fb8549b978
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2025-12-11 01:20:43 +1100

    python: updated palette handling after recent fix in halo firmware 0.6.5. Added extra bitmap/custom palette examples

--- python/packages/frame_ble/examples/reset_palette.py
+++ python/packages/frame_ble/examples/reset_palette.py
@@ -6,33 +6,49 @@ async def main():
 
     try:
         await frame.connect()
+        # stop any application, if running, so we can send lua commands
+        await frame.send_break_signal()
+        frame._user_print_response_handler = print
 
         if frame.type == BrilliantDeviceType.FRAME:
-            # stop any application, if running, so we can send lua commands
-            await frame.send_break_signal()
-
             # Set the palette back to the firmware default
-            await frame.send_lua("frame.display.assign_color_ycbcr(1, 0, 4, 4);print(0)", await_print=True) # VOID
-            await frame.send_lua("frame.display.assign_color_ycbcr(2, 15, 4, 4);print(0)", await_print=True) # WHITE
-            await frame.send_lua("frame.display.assign_color_ycbcr(3, 7, 4, 4);print(0)", await_print=True) # GREY
-            await frame.send_lua("frame.display.assign_color_ycbcr(4, 5, 3, 6);print(0)", await_print=True) # RED
-            await frame.send_lua("frame.display.assign_color_ycbcr(5, 9, 3, 5);print(0)", await_print=True) # PINK
-            await frame.send_lua("frame.display.assign_color_ycbcr(6, 2, 2, 5);print(0)", await_print=True) # DARKBROWN
-            await frame.send_lua("frame.display.assign_color_ycbcr(7, 4, 2, 5);print(0)", await_print=True) # BROWN
-            await frame.send_lua("frame.display.assign_color_ycbcr(8, 9, 2, 5);print(0)", await_print=True) # ORANGE
-            await frame.send_lua("frame.display.assign_color_ycbcr(9, 13, 2, 4);print(0)", await_print=True) # YELLOW
-            await frame.send_lua("frame.display.assign_color_ycbcr(10, 4, 4, 3);print(0)", await_print=True) # DARKGREEN
-            await frame.send_lua("frame.display.assign_color_ycbcr(11, 6, 2, 3);print(0)", await_print=True) # GREEN
-            await frame.send_lua("frame.display.assign_color_ycbcr(12, 10, 1, 3);print(0)", await_print=True) # LIGHTGREEN
-            await frame.send_lua("frame.display.assign_color_ycbcr(13, 1, 5, 2);print(0)", await_print=True) # NIGHTBLUE
-            await frame.send_lua("frame.display.assign_color_ycbcr(14, 4, 5, 2);print(0)", await_print=True) # SEABLUE
-            await frame.send_lua("frame.display.assign_color_ycbcr(15, 8, 5, 2);print(0)", await_print=True) # SKYBLUE
-            await frame.send_lua("frame.display.assign_color_ycbcr(16, 13, 4, 3);print(0)", await_print=True) # CLOUDBLUE
+            await frame.send_lua("frame.display.assign_color_ycbcr('VOID', 0, 4, 4);print(0)", await_print=True) # VOID
+            await frame.send_lua("frame.display.assign_color_ycbcr('WHITE', 15, 4, 4);print(0)", await_print=True) # WHITE
+            await frame.send_lua("frame.display.assign_color_ycbcr('GREY', 7, 4, 4);print(0)", await_print=True) # GREY
+            await frame.send_lua("frame.display.assign_color_ycbcr('RED', 5, 3, 6);print(0)", await_print=True) # RED
+            await frame.send_lua("frame.display.assign_color_ycbcr('PINK', 9, 3, 5);print(0)", await_print=True) # PINK
+            await frame.send_lua("frame.display.assign_color_ycbcr('DARKBROWN', 2, 2, 5);print(0)", await_print=True) # DARKBROWN
+            await frame.send_lua("frame.display.assign_color_ycbcr('BROWN', 4, 2, 5);print(0)", await_print=True) # BROWN
+            await frame.send_lua("frame.display.assign_color_ycbcr('ORANGE', 9, 2, 5);print(0)", await_print=True) # ORANGE
+            await frame.send_lua("frame.display.assign_color_ycbcr('YELLOW', 13, 2, 4);print(0)", await_print=True) # YELLOW
+            await frame.send_lua("frame.display.assign_color_ycbcr('DARKGREEN', 4, 4, 3);print(0)", await_print=True) # DARKGREEN
+            await frame.send_lua("frame.display.assign_color_ycbcr('GREEN', 6, 2, 3);print(0)", await_print=True) # GREEN
+            await frame.send_lua("frame.display.assign_color_ycbcr('LIGHTGREEN', 10, 1, 3);print(0)", await_print=True) # LIGHTGREEN
+            await frame.send_lua("frame.display.assign_color_ycbcr('NIGHTBLUE', 1, 5, 2);print(0)", await_print=True) # NIGHTBLUE
+            await frame.send_lua("frame.display.assign_color_ycbcr('SEABLUE', 4, 5, 2);print(0)", await_print=True) # SEABLUE
+            await frame.send_lua("frame.display.assign_color_ycbcr('SKYBLUE', 8, 5, 2);print(0)", await_print=True) # SKYBLUE
+            await frame.send_lua("frame.display.assign_color_ycbcr('CLOUDBLUE', 13, 4, 3);print(0)", await_print=True) # CLOUDBLUE
             print("Default palette set.")
 
-            #await frame.send_lua("frame.display.text('Hello, World!', 50, 100, {color='ORANGE'});frame.display.show();print(0)", await_print=True)
         else:
-            print("This script is intended for the Frame device only.")
+            # Set the palette back to the firmware default
+            # Halo also supports the color name string, but we can also use index values 0..15
+            await frame.send_lua("frame.display.assign_color_ycbcr(0, 0, 4, 4);print(0)", await_print=True) # VOID
+            await frame.send_lua("frame.display.assign_color_ycbcr(1, 15, 4, 4);print(0)", await_print=True) # WHITE
+            await frame.send_lua("frame.display.assign_color_ycbcr(2, 7, 4, 4);print(0)", await_print=True) # GREY
+            await frame.send_lua("frame.display.assign_color_ycbcr(3, 5, 3, 6);print(0)", await_print=True) # RED
+            await frame.send_lua("frame.display.assign_color_ycbcr(4, 9, 3, 5);print(0)", await_print=True) # PINK
+            await frame.send_lua("frame.display.assign_color_ycbcr(5, 2, 2, 5);print(0)", await_print=True) # DARKBROWN
+            await frame.send_lua("frame.display.assign_color_ycbcr(6, 4, 2, 5);print(0)", await_print=True) # BROWN
+            await frame.send_lua("frame.display.assign_color_ycbcr(7, 9, 2, 5);print(0)", await_print=True) # ORANGE
+            await frame.send_lua("frame.display.assign_color_ycbcr(8, 13, 2, 4);print(0)", await_print=True) # YELLOW
+            await frame.send_lua("frame.display.assign_color_ycbcr(9, 4, 4, 3);print(0)", await_print=True) # DARKGREEN
+            await frame.send_lua("frame.display.assign_color_ycbcr(10, 6, 2, 3);print(0)", await_print=True) # GREEN
+            await frame.send_lua("frame.display.assign_color_ycbcr(11, 10, 1, 3);print(0)", await_print=True) # LIGHTGREEN
+            await frame.send_lua("frame.display.assign_color_ycbcr(12, 1, 5, 2);print(0)", await_print=True) # NIGHTBLUE
+            await frame.send_lua("frame.display.assign_color_ycbcr(13, 4, 5, 2);print(0)", await_print=True) # SEABLUE
+            await frame.send_lua("frame.display.assign_color_ycbcr(14, 8, 5, 2);print(0)", await_print=True) # SKYBLUE
+            await frame.send_lua("frame.display.assign_color_ycbcr(15, 13, 4, 3);print(0)", await_print=True) # CLOUDBLUE
 
         await frame.disconnect()
 
