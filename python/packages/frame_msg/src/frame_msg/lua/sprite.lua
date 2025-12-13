afdd878147c77750654bfc7980ccf3380176ed73
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2025-12-13 16:43:43 +1100

    python/flutter: updated sprite.lua to refer to palette entries on Halo as 0-15 rather than 1-16 after firmware fix

--- python/packages/frame_msg/src/frame_msg/lua/sprite.lua
+++ python/packages/frame_msg/src/frame_msg/lua/sprite.lua
@@ -22,12 +22,12 @@ function _M.set_palette(num_colors, palette_data)
 	for i=1,num_colors do
 		local col_offset = (i - 1) * 3
 
-		-- Frame indexes are the color names, Halo indexes are 1-16
+		-- Frame indexes are the color names, Halo indexes are 0-15
 		local index
 		if frame.HARDWARE_VERSION == 'Frame' then
 			index = colors[i]
 		else
-			index = i
+			index = i - 1
 		end
 
 		frame.display.assign_color(index,
