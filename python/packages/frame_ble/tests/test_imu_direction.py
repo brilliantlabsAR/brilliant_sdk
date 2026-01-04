c6a74bd032d453282fe49acd8e1acef66da210ed
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2026-01-04 13:50:45 +1100

    python: tidy up imu tests

--- python/packages/frame_ble/tests/test_imu_direction.py
+++ python/packages/frame_ble/tests/test_imu_direction.py
@@ -9,24 +9,23 @@ from frame_ble import FrameBle
 async def main():
     b = FrameBle()
 
-    await b.connect(print_response_handler=lambda s: print(s))
+    try:
+        await b.connect(print_response_handler=lambda s: print(s))
 
-    # Load the I2C bus
-    await b.send_lua("frame.camera.auto(true, 'average')")
+        # Enable taps
+        await b.send_lua("frame.imu.tap_callback((function()print('Tap!')end))")
 
-    # Enable taps
-    await b.send_lua("frame.imu.tap_callback((function()print('Tap!')end))")
+        while True:
+            await b.send_lua("resp = frame.imu.direction()")
 
-    while True:
-        await b.send_lua("resp = frame.imu.direction()")
-
-        await b.send_lua(
-            "print('roll: '..tostring(resp['roll'])..'\tpitch: '..tostring(resp['pitch'])..'\theading: '..tostring(resp['heading']))",
-            await_print=True,
-        )
-        await asyncio.sleep(0.1)
-
-    await b.disconnect()
-
+            await b.send_lua(
+                "print('roll: '..tostring(resp['roll'])..'\tpitch: '..tostring(resp['pitch'])..'\theading: '..tostring(resp['heading']))",
+                await_print=True,
+            )
+            await asyncio.sleep(0.1)
+    except asyncio.CancelledError:
+        pass
+    finally:
+        await b.disconnect()
 
 asyncio.run(main())
--- python/packages/frame_ble/tests/test_imu_raw.py
+++ python/packages/frame_ble/tests/test_imu_raw.py
@@ -9,25 +9,24 @@ from frame_ble import FrameBle
 async def main():
     b = FrameBle()
 
-    await b.connect(print_response_handler=lambda s: print(s))
+    try:
+        await b.connect(print_response_handler=lambda s: print(s))
 
-    # Load the I2C bus
-    await b.send_lua("frame.camera.auto(true, 'average')")
+        # Enable taps
+        await b.send_lua("frame.imu.tap_callback((function()print('Tap!')end))")
 
-    # Enable taps
-    await b.send_lua("frame.imu.tap_callback((function()print('Tap!')end))")
+        while True:
+            print("Accelerometer\t\t\t\t\t\t\t\tCompass")
+            await b.send_lua("resp = frame.imu.raw()")
 
-    while True:
-        print("Accelerometer\t\t\t\t\t\t\t\tCompass")
-        await b.send_lua("resp = frame.imu.raw()")
-
-        await b.send_lua(
-            "print(tostring(resp['accelerometer']['x'])..'\t'..tostring(resp['accelerometer']['y'])..'\t'..tostring(resp['accelerometer']['z'])..'\t'..tostring(resp['compass']['x'])..'\t'..tostring(resp['compass']['y'])..'\t'..tostring(resp['compass']['z']))",
-            await_print=True,
-        )
-        await asyncio.sleep(0.1)
-
-    await b.disconnect()
-
+            await b.send_lua(
+                "print(tostring(resp['accelerometer']['x'])..'\t'..tostring(resp['accelerometer']['y'])..'\t'..tostring(resp['accelerometer']['z'])..'\t'..tostring(resp['compass']['x'])..'\t'..tostring(resp['compass']['y'])..'\t'..tostring(resp['compass']['z']))",
+                await_print=True,
+            )
+            await asyncio.sleep(0.1)
+    except asyncio.CancelledError:
+        pass
+    finally:
+        await b.disconnect()
 
 asyncio.run(main())
