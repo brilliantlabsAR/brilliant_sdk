841701aa7d4abc595c3cd5db47ce0b9fffa45744
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2026-02-24 15:56:50 +1100

    flutter: updated text_sprite_block - BREAKING

--- flutter/packages/frame_msg/lib/tx/text_sprite_block.dart
+++ flutter/packages/frame_msg/lib/tx/text_sprite_block.dart
@@ -21,16 +21,18 @@ import 'sprite.dart';
 class TxTextSpriteBlock extends TxMsg {
   final int _width;
   int get width => _width;
+  final int _lineHeight;
+  int get lineHeight => _lineHeight;
   final int _fontSize;
   int get fontSize => _fontSize;
-  final int _maxDisplayRows;
-  int get maxDisplayRows => _maxDisplayRows;
-  final List<TxSprite> _sprites = [];
-  List<TxSprite> get rasterizedSprites => _sprites;
-
-  late final ui.Paragraph _paragraph;
-  late final List<ui.LineMetrics> _lineMetrics;
-  int get numLines => _lineMetrics.length;
+  final int _maxDisplayLines;
+  int get maxDisplayRows => _maxDisplayLines;
+  final ui.TextAlign _textAlign;
+  ui.TextAlign get textAlign => _textAlign;
+  final ui.TextDirection _textDirection;
+  ui.TextDirection get textDirection => _textDirection;
+  final String? _fontFamily;
+  String? get fontFamily => _fontFamily;
 
   static img.PaletteUint8? monochromePal;
 
@@ -45,25 +47,29 @@ class TxTextSpriteBlock extends TxMsg {
     return monochromePal!;
   }
 
-  /// After construction, a TextSpriteBlock should be tested that it has a non-zero number of
-  /// sprite lines to send, otherwise it should not be rasterized nor sent
-  bool get isEmpty => _lineMetrics.isEmpty;
-
-  /// After construction, a TextSpriteBlock should be tested that it has a non-zero number of
-  /// sprite lines to send, otherwise it should not be rasterized nor sent
-  bool get isNotEmpty => _lineMetrics.isNotEmpty;
-
   TxTextSpriteBlock({
-      required int width,
-      required int fontSize,
-      required int maxDisplayRows,
+      int width = 200,
+      int lineHeight = 16,
+      int fontSize = 12,
+      int maxDisplayLines = 3,
       String? fontFamily,
       ui.TextAlign textAlign = ui.TextAlign.left,
-      ui.TextDirection textDirection = ui.TextDirection.ltr,
-      required String text})
+      ui.TextDirection textDirection = ui.TextDirection.ltr})
       : _width = width,
         _fontSize = fontSize,
-        _maxDisplayRows = maxDisplayRows {
+        _maxDisplayLines = maxDisplayLines,
+        _fontFamily = fontFamily,
+        _textAlign = textAlign,
+        _textDirection = textDirection,
+        _lineHeight = lineHeight;
+
+  /// Since the Paragraph rasterizing to the Canvas, and the getting of the Image bytes
+  /// are async functions, there needs to be an async function not just the constructor.
+  /// Plus we want the caller to decide how many lines of a long paragraph to rasterize, and when.
+  /// Text lines as TxSprites are returned as a List, and the caller can decide how many to send to Frame, and when.
+  Future<List<TxSprite>> createTextSprites(String text) async {
+    final List<TxSprite> sprites = [];
+
     final paragraphBuilder = ui.ParagraphBuilder(ui.ParagraphStyle(
       textAlign: textAlign,
       textDirection: textDirection,
@@ -72,126 +78,119 @@ class TxTextSpriteBlock extends TxMsg {
     ));
 
     paragraphBuilder.addText(text);
-    _paragraph = paragraphBuilder.build();
+    final ui.Paragraph paragraph = paragraphBuilder.build();
 
-    _paragraph.layout(ui.ParagraphConstraints(width: width.toDouble()));
+    paragraph.layout(ui.ParagraphConstraints(width: width.toDouble()));
 
     // work out height using metrics after paragraph.layout() call
-    _lineMetrics = _paragraph.computeLineMetrics();
-  }
+    List<LineMetrics> lineMetrics= paragraph.computeLineMetrics();
 
-  /// Since the Paragraph rasterizing to the Canvas, and the getting of the Image bytes
-  /// are async functions, there needs to be an async function not just the constructor.
-  /// Plus we want the caller to decide how many lines of a long paragraph to rasterize, and when.
-  /// Text lines as TxSprites are accumulated in this object.
-  /// startLine and endLine are inclusive
-  Future<void> rasterize({required int startLine, required int endLine}) async {
-    if (isNotEmpty) {
+    if (lineMetrics.isEmpty) {
+      return sprites;
+    }
 
-      if (startLine < 0 || startLine > _lineMetrics.length - 1) throw Exception('startLine must be > 0 and < ${_lineMetrics.length}');
-      if (endLine < startLine || endLine > _lineMetrics.length - 1) throw Exception('endLine must be >= startLine and < ${_lineMetrics.length}');
+    final endIndex = lineMetrics.length - 1;
 
-      // Calculate the top and bottom boundaries for the selected lines
-      double topBoundary = 0;
-      double bottomBoundary = 0;
+    // Calculate the top and bottom boundaries for the selected lines
+    double topBoundary = 0;
+    double bottomBoundary = 0;
 
-      // work out a clip rectangle so we only draw the lines between startLine and endLine
-      topBoundary = _lineMetrics[startLine].baseline - _lineMetrics[startLine].ascent;
-      bottomBoundary = _lineMetrics[endLine].baseline + _lineMetrics[endLine].descent;
+    // work out a clip rectangle
+    topBoundary = lineMetrics[0].baseline - lineMetrics[0].ascent;
+    bottomBoundary = lineMetrics[endIndex].baseline + lineMetrics[endIndex].descent;
 
-      // Define the area to clip: a window over the selected lines
-      final clipRect = Rect.fromLTWH(
-        0, // Start from the left edge of the canvas
-        topBoundary, // Start clipping from the top of the startLine
-        width.toDouble(), // Full width of the paragraph
-        bottomBoundary - topBoundary, // Height of the selected lines
-      );
+    // Define the area to clip: a window over the selected lines
+    final clipRect = Rect.fromLTWH(
+      0, // Start from the left edge of the canvas
+      topBoundary, // Start clipping from the top of the startLine
+      width.toDouble(), // Full width of the paragraph
+      bottomBoundary - topBoundary, // Height of the selected lines
+    );
 
-      final pictureRecorder = ui.PictureRecorder();
-      final canvas = ui.Canvas(pictureRecorder);
-      canvas.clipRect(clipRect);
+    final pictureRecorder = ui.PictureRecorder();
+    final canvas = ui.Canvas(pictureRecorder);
+    canvas.clipRect(clipRect);
 
-      canvas.drawParagraph(_paragraph, ui.Offset.zero);
-      final ui.Picture picture = pictureRecorder.endRecording();
+    canvas.drawParagraph(paragraph, ui.Offset.zero);
+    final ui.Picture picture = pictureRecorder.endRecording();
 
-      final int totalHeight = (bottomBoundary - topBoundary).toInt();
-      final int topOffset = topBoundary.toInt();
+    final int totalHeight = (bottomBoundary - topBoundary).toInt();
+    final int topOffset = topBoundary.toInt();
 
-      final ui.Image image = await picture.toImage(_width, totalHeight);
+    final ui.Image image = await picture.toImage(_width, totalHeight);
 
-      var byteData =
-          (await image.toByteData(format: ui.ImageByteFormat.rawUnmodified))!;
+    var byteData =
+        (await image.toByteData(format: ui.ImageByteFormat.rawUnmodified))!;
 
-      // loop over each requested line of text in the paragraph and create a TxSprite
-      for (var line in _lineMetrics.sublist(startLine, endLine + 1)) {
-        final int tlX = line.left.toInt();
-        final int tlY = (line.baseline - line.ascent).toInt();
-        final int tlyShifted = tlY - topOffset;
-        int lineWidth = line.width.toInt();
-        int lineHeight = (line.ascent + line.descent).toInt();
+    // loop over each requested line of text in the paragraph and create a TxSprite
+    for (var line in lineMetrics) {
+      final int tlX = line.left.toInt();
+      final int tlY = (line.baseline - line.ascent).toInt();
+      final int tlyShifted = tlY - topOffset;
+      int lineWidth = line.width.toInt();
+      int lineHeight = (line.ascent + line.descent).toInt();
 
-        // check for non-blank lines
-        if (lineWidth > 0 && lineHeight > 0) {
-          var linePixelData = Uint8List(lineWidth * lineHeight);
+      // check for non-blank lines
+      if (lineWidth > 0 && lineHeight > 0) {
+        var linePixelData = Uint8List(lineWidth * lineHeight);
 
-          for (int i = 0; i < lineHeight; i++) {
-            // take one row of the source image byteData, remembering it's in RGBA so 4 bytes per pixel
-            // and remembering the origin of the image is the top of the startLine, so we need to
-            // shift all the top-left Ys by that first Y offset.
-            var sourceRow = byteData.buffer
-                .asUint8List(((tlyShifted + i) * _width + tlX) * 4, lineWidth * 4);
+        for (int i = 0; i < lineHeight; i++) {
+          // take one row of the source image byteData, remembering it's in RGBA so 4 bytes per pixel
+          // and remembering the origin of the image is the top of the startLine, so we need to
+          // shift all the top-left Ys by that first Y offset.
+          var sourceRow = byteData.buffer
+              .asUint8List(((tlyShifted + i) * _width + tlX) * 4, lineWidth * 4);
 
-            for (int j = 0; j < lineWidth; j++) {
-              // take only every 4th byte because the source buffer is RGBA
-              // and map it to palette index 1 if it's 128 or bigger (monochrome palette only, and text rendering will be anti-aliased)
-              linePixelData[i * lineWidth + j] = sourceRow[4 * j] >= 128 ? 1 : 0;
-            }
+          for (int j = 0; j < lineWidth; j++) {
+            // take only every 4th byte because the source buffer is RGBA
+            // and map it to palette index 1 if it's 128 or bigger (monochrome palette only, and text rendering will be anti-aliased)
+            linePixelData[i * lineWidth + j] = sourceRow[4 * j] >= 128 ? 1 : 0;
           }
+        }
 
-          // make a Sprite out of the line and add to the list
-          _sprites.add(TxSprite(
-            width: lineWidth,
-            height: lineHeight,
-            numColors: 2,
-            paletteData: _getPalette().data,
-            pixelData: linePixelData
-          ));
-        }
-        else {
-          // zero-width line, a blank line in the text block
-          // so we make a 1x1 px sprite in the void color
-          _sprites.add(TxSprite(
-            width: 1,
-            height: 1,
-            numColors: 2,
-            paletteData: _getPalette().data,
-            pixelData: Uint8List(1)
-          ));
-        }
+        // make a Sprite out of the line and add to the list
+        sprites.add(TxSprite(
+          width: lineWidth,
+          height: lineHeight,
+          numColors: 2,
+          paletteData: _getPalette().data,
+          pixelData: linePixelData
+        ));
+      }
+      else {
+        // zero-width line, a blank line in the text block
+        // so we make a 1x1 px sprite in the void color
+        sprites.add(TxSprite(
+          width: 1,
+          height: 1,
+          numColors: 2,
+          paletteData: _getPalette().data,
+          pixelData: Uint8List(1)
+        ));
       }
     }
+
+    return sprites;
   }
 
   /// Convert TxTextSpriteBlock back to a single image for testing/verification
   /// startLine and endLine are inclusive
-  Future<Uint8List> toPngBytes({required int startLine, required int endLine}) async {
-    if (_sprites.isEmpty) {
-      throw Exception('_sprites is empty: call rasterize() before toPngBytes()');
+  Future<Uint8List> toPngBytes({required List<TxSprite> rasterizedSprites}) async {
+    if (rasterizedSprites.isEmpty) {
+      throw Exception('rasterizedSprites is empty');
     }
 
-    // work out which range of lines we're drawing, and shift up by topOffset in Y
-    final double topBoundary = _lineMetrics[startLine].baseline - _lineMetrics[startLine].ascent;
-    final double bottomBoundary = _lineMetrics[endLine].baseline + _lineMetrics[endLine].descent;
-    final int totalHeight = (bottomBoundary - topBoundary).toInt();
-    final int topOffset = topBoundary.toInt();
+    // use the heights of the TxSprites to compose the image
+    int totalHeight = rasterizedSprites.fold(0, (sum, sprite) => sum + sprite.height);
 
     // create an image for the whole block
     var preview = img.Image(width: width, height: totalHeight);
 
     // copy in each of the sprites
-    for (int i = startLine; i <= endLine; i++) {
-      img.compositeImage(preview, rasterizedSprites[i].toImage(),
-          dstY: (_lineMetrics[i].baseline - _lineMetrics[i].ascent - topOffset).toInt());
+    int currentY = 0;
+    for (TxSprite sprite in rasterizedSprites) {
+      img.compositeImage(preview, sprite.toImage(), dstY: currentY);
+      currentY += sprite.height;
     }
 
     return img.encodePng(preview);
@@ -200,34 +199,19 @@ class TxTextSpriteBlock extends TxMsg {
   /// Corresponding parser should be called from frame_app data handler
   @override
   Uint8List pack() {
-    if (_sprites.isEmpty) {
-      throw Exception('_sprites is empty: call rasterize() before pack()');
-    }
-
     int widthMsb = _width >> 8;
     int widthLsb = _width & 0xFF;
-
-    // store the x (16-bit) and y (16-bit) offsets as pairs for each of the lines
-    Uint8List offsets = Uint8List(_lineMetrics.length * 4);
-
-    for (int i = 0; i < _lineMetrics.length; i++) {
-      var lm = _lineMetrics[i];
-      int xOffset = lm.left.toInt();
-      int yOffset = (lm.baseline - lm.ascent).toInt();
-      offsets[4 * i] = xOffset >> 8;
-      offsets[4 * i + 1] = xOffset & 0xFF;
-      offsets[4 * i + 2] = yOffset >> 8;
-      offsets[4 * i + 3] = yOffset & 0xFF;
-    }
+    int lineHeightMsb = _lineHeight >> 8;
+    int lineHeightLsb = _lineHeight & 0xFF;
 
     // special marker for Block header 0xFF, width of the block, max display rows, num lines, offsets within block for each line
     return Uint8List.fromList([
       0xFF,
       widthMsb,
       widthLsb,
-      _maxDisplayRows & 0xFF,
-      _sprites.length & 0xFF,
-      ...offsets
+      lineHeightMsb,
+      lineHeightLsb,
+      _maxDisplayLines & 0xFF
     ]);
   }
 }
